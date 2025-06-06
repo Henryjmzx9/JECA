0f457c09dbc333d574ed4f977d8f6e909eba5b33
Anderson Javier Galicia Ramírez <andyssjrmz.18@gmail.com>
2025-06-06 16:50:21 -0600

    First commit i love i loveeeee

--- src/esfe/java/persistencia/Main.java
+++ src/esfe/java/persistencia/Main.java
@@ -8,6 +8,7 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Holaaaaaaaaa!");
         System.out.println("hola menoles!");
+        System.out.println("hellouuuuuu!");
         int a =1;
         int b =2;
         int resultad= a+b;
