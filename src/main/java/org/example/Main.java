2ac483d6352c378e51c1da37d1ca52b94f9489be
Marcos Vladimir Calzadilla Gutiérrez <marcosgtzx7@hotmail.com>
2025-06-06 16:22:32 -0600

    ola eu so do brazil

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,7 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Holaaaaaaaaa!");
+        System.out.println("hola menoles!");
         int a =1;
         int b =2;
         int resultad= a+b;
