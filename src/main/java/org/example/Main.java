e0eef9ec8718ab6069cba54129146a3df162a222
Henry Eduardo Jimenez Maye <jm25002@esfe.agape.edu.sv>
2025-06-05 23:24:18 -0600

    Prueba cambio de nombre

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,11 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+        System.out.println("Holaaaaaaaaa!");
+        int a =1;
+        int b =2;
+        int resultad= a+b;
+        System.out.println(resultad);
 
 
     }
