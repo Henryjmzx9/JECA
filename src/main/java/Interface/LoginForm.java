00595426aefcae207af1ee65940278e3422bdeeb
Marcos Vladimir Calzadilla Gutiérrez <cg25003@esfe.agape.edu.sv>
2025-06-10 15:19:20 -0600

    Se terminó de configurar la vista LoginForm

--- src/main/java/Interface/LoginForm.java
+++ src/main/java/Interface/LoginForm.java
@@ -3,4 +3,11 @@ package Interface;
 import javax.swing.*;
 
 public class LoginForm extends JDialog {
+    private JPanel mainPanel;
+    private JTextField textField1;
+    private JPasswordField passwordField1;
+    private JButton btnLogin;
+    private JButton btnSalir;
+    private JLabel TxtEmail;
+    private JLabel txtPassword;
 }
