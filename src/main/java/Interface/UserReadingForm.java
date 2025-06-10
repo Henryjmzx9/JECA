1295b0cba4ef3b5dbbe7b94601f2919fb2c22237
Marcos Vladimir Calzadilla Gutiérrez <cg25003@esfe.agape.edu.sv>
2025-06-10 14:36:01 -0600

    Se crearon las demás vistas 4 y 5

---
+++ src/main/java/Interface/UserReadingForm.java
@@ -0,0 +1,6 @@
+package Interface;
+
+import javax.swing.*;
+
+public class UserReadingForm extends JDialog {
+}
---
+++ src/main/java/Interface/UserWriteForm.java
@@ -0,0 +1,6 @@
+package Interface;
+
+import javax.swing.*;
+
+public class UserWriteForm extends JDialog {
+}
