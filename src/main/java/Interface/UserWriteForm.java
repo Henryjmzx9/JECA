dc63b7c1424160b10e16a68371044297b437d932
Anderssngalicia06 <andyssjrmz.18@gmail.com>
2025-06-10 23:52:02 -0600

    Se creo el formulario UserWrite

--- src/main/java/Interface/UserWriteForm.java
+++ src/main/java/Interface/UserWriteForm.java
@@ -3,4 +3,11 @@ package Interface;
 import javax.swing.*;
 
 public class UserWriteForm extends JDialog {
+    private JPanel mainPanel;
+    private JTextField txtName;
+    private JTextField txtEmail;
+    private JPasswordField txtPassword;
+    private JComboBox cbStatus;
+    private JButton btnOk;
+    private JButton btnCancel;
 }
