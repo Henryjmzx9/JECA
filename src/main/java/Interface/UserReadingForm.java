106176fd00d912e7f2dd84f194e379d1b85ad7b2
Anderssngalicia06 <andyssjrmz.18@gmail.com>
2025-06-10 23:53:13 -0600

    Se creo el formulario Reading

--- src/main/java/Interface/UserReadingForm.java
+++ src/main/java/Interface/UserReadingForm.java
@@ -3,4 +3,10 @@ package Interface;
 import javax.swing.*;
 
 public class UserReadingForm extends JDialog {
+    private JPanel mainPanel;
+    private JButton btnCreate;
+    private JTextField txtName;
+    private JTable tableUsers;
+    private JButton btnUpdate;
+    private JButton btnDelete;
 }
