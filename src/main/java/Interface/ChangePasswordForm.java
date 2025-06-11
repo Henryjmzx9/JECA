f62a9fd4cc3a100e328dedbcdf629f1e8e942a33
Anderssngalicia06 <andyssjrmz.18@gmail.com>
2025-06-10 23:54:22 -0600

    Se creo el formulario Cambiar Contraseña

--- src/main/java/Interface/ChangePasswordForm.java
+++ src/main/java/Interface/ChangePasswordForm.java
@@ -3,4 +3,8 @@ package Interface;
 import javax.swing.*;
 
 public class ChangePasswordForm extends JDialog {
+    private JPanel mainPanel;
+    private JTextField txtEmail;
+    private JPasswordField txtPassword;
+    private JButton btnChangePassword;
 }
