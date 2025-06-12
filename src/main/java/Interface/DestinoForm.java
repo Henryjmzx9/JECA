86a9cd3550331abf86440f6436bae00d1bce9289
Marcos Vladimir Calzadilla Gutiérrez <cg25003@esfe.agape.edu.sv>
2025-06-11 23:34:43 -0600

    Creacion de vista Destino

--- src/main/java/Interface/DestinoForm.java
+++ src/main/java/Interface/DestinoForm.java
@@ -1,4 +1,12 @@
 package Interface;
 
+import javax.swing.*;
+
 public class DestinoForm {
+    private JPanel Destinos;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton cargarImagenButton;
+    private JTextArea textArea1;
+    private JTextField textField3;
 }
