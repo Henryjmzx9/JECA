093747cd0569ee58f04204cd990dcc55dbff2aaf
Marcos Vladimir Calzadilla Gutiérrez <cg25003@esfe.agape.edu.sv>
2025-06-11 23:38:03 -0600

    Creacion de vista paquete la clase

--- src/main/java/Interface/PaqueteViajeForm.java
+++ src/main/java/Interface/PaqueteViajeForm.java
@@ -1,4 +1,13 @@
 package Interface;
 
+import javax.swing.*;
+
 public class PaqueteViajeForm {
+    private JPanel Paquetes;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextArea textArea1;
+    private JSpinner spinner1;
+    private JComboBox comboBox1;
+    private JButton guardarButton;
 }
