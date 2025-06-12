c9d4b9cdb9121337f5c383bfee69a4b189945eb6
Marcos Vladimir Calzadilla Gutiérrez <cg25003@esfe.agape.edu.sv>
2025-06-11 23:36:31 -0600

    Creacion de vista Pago

--- src/main/java/Interface/PagoForm.java
+++ src/main/java/Interface/PagoForm.java
@@ -1,4 +1,10 @@
 package Interface;
 
+import javax.swing.*;
+
 public class PagoForm {
+    private JPanel Pagos;
+    private JComboBox comboBox1;
+    private JComboBox comboBox2;
+    private JTextField textField1;
 }
