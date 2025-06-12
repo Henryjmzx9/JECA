eefc9be43efd6c0634283784f88a4cdce080108b
Marcos Vladimir Calzadilla Gutiérrez <cg25003@esfe.agape.edu.sv>
2025-06-11 23:29:04 -0600

    Creacion de vista cliente

--- src/main/java/Interface/ClientForm.java
+++ src/main/java/Interface/ClientForm.java
@@ -1,4 +1,10 @@
 package Interface;
 
+import javax.swing.*;
+
 public class ClientForm {
+    private JLabel Cliente;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton guardarCambiosButton;
 }
