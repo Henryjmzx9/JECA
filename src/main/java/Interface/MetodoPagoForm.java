82886be9c46cf2c455f103f4c42eeba6160d06b0
Marcos Vladimir Calzadilla Gutiérrez <cg25003@esfe.agape.edu.sv>
2025-06-11 23:35:35 -0600

    Creacion de vista MetodoPago

--- src/main/java/Interface/MetodoPagoForm.java
+++ src/main/java/Interface/MetodoPagoForm.java
@@ -1,4 +1,11 @@
 package Interface;
 
+import javax.swing.*;
+
 public class MetodoPagoForm {
+    private JPanel MétodoPago;
+    private JTextField textField1;
+    private JButton actualizarButton;
+    private JButton agregarButton;
+    private JButton eliminarButton;
 }
