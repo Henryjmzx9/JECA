a92fa923b46f0916a2b88f428982e62c074f195a
Marcos Vladimir Calzadilla Gutiérrez <cg25003@esfe.agape.edu.sv>
2025-06-11 23:39:44 -0600

    Se modificó de vista ReservaClase

--- src/main/java/Interface/ReservaAdminForm.java
+++ src/main/java/Interface/ReservaAdminForm.java
@@ -1,4 +1,11 @@
 package Interface;
 
+import javax.swing.*;
+
 public class ReservaAdminForm {
+    private JPanel Reserva;
+    private JComboBox comboBox1;
+    private JTextField textField1;
+    private JComboBox comboBox2;
+    private JButton guardarButton;
 }
