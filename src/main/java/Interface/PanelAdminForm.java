2b1f35bccd1455aef1fd29fae811b764800e3d23
Kevin Jose Mendoza Tobar <mt25001@esfe.agape.edu.sv>
2025-06-13 16:17:25 -0600

    Se creo el panel principal de del administrador

---
+++ src/main/java/Interface/PanelAdminForm.java
@@ -0,0 +1,13 @@
+package Interface;
+
+import javax.swing.*;
+
+public class PanelAdminForm {
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
+    private JButton button7;
+}
