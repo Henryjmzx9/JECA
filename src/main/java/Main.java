8413c0141d25bf53df103ba976b70ee42c24489a
Marcos Vladimir Calzadilla Gutiérrez <cg25003@esfe.agape.edu.sv>
2025-06-13 23:52:22 -0600

    Clase main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,22 @@
 
-//TlIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+
+import Interface.LoginForm;
+import Interface.MainForm;
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Holaaaaaaaaa!");
-        System.out.println("hola menoles!");
-        System.out.println("hellouuuuuu!");
-        System.out.println("Tudo bem?!");
-        int a =1;
-        int b =2;
-        int resultad= a+b;
-        System.out.println(resultad);
-        System.out.println("Hola, mundo!");
+
+        // Utiliza el Event Dispatch Thread (EDT) para asegurar la seguridad en la ejecución de la interfaz gráfica
+        SwingUtilities.invokeLater(() -> {
+            // Crea e instancia la ventana principal de la aplicación
+            MainForm mainForm = new MainForm();
+            mainForm.setVisible(true); // Se muestra maximizada (ya configurado en MainForm)
+
+            // Crea e instancia la ventana de inicio de sesión, pasándole la ventana principal como referencia
+            LoginForm loginForm = new LoginForm(mainForm);
+            loginForm.setVisible(true); // Se muestra primero, solicitando credenciales
+        });
     }
 }
