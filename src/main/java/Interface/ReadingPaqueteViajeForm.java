c4b9b7c9ec25dfb5312cc0a5cbb04f3d38fbc2da
Anderson Javier Galicia Ramírez <gr25004@esfe.agape.edu.sv>
2025-06-14 21:52:12 -0600

    Se crearon las interfaces restantes

---
+++ src/main/java/Interface/ReadingPaqueteViajeForm.java
@@ -0,0 +1,14 @@
+package Interface;
+
+import javax.swing.*;
+
+public class ReadingPaqueteViajeForm {
+    private JTextArea textArea1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JComboBox comboBox1;
+    private JSpinner spinner1;
+    private JButton btnGuardar;
+    private JButton btnEliminar;
+    private JButton btnEditar;
+}
---
+++ src/main/java/Interface/clientReadingForm.java
@@ -0,0 +1,14 @@
+package Interface;
+
+import javax.swing.*;
+
+public class clientReadingForm {
+    private JTextField txtPhoneClient;
+    private JTextField txtEmailClient;
+    private JTextField txtAddresClient;
+    private JTextField txtNameClient;
+    private JButton btnGuardar;
+    private JButton btnEliminar;
+    private JButton btnEditar;
+    private JTable tableClient;
+}
