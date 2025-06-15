8c5c08853009ac07f4a880928380d5f76fb4fc78
Anderson Javier Galicia Ramírez <gr25004@esfe.agape.edu.sv>
2025-06-14 22:59:07 -0600

    Se creo el MetodoPagoReadin

---
+++ src/main/java/Interface/MetodoPagoReadingForm.java
@@ -0,0 +1,11 @@
+package Interface;
+
+import javax.swing.*;
+
+public class MetodoPagoReadingForm {
+    private JTextField txtNombre;
+    private JButton btnEliminar;
+    private JButton btnModificar;
+    private JButton btnCrear;
+    private JTable tableMetodoPago;
+}
