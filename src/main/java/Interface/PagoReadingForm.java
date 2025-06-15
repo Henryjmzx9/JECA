f305265f9cdb6f885b84178bead5fcdc8204f02f
Anderson Javier Galicia Ramírez <gr25004@esfe.agape.edu.sv>
2025-06-15 11:36:04 -0600

    PagoReading se creo

---
+++ src/main/java/Interface/PagoReadingForm.java
@@ -0,0 +1,11 @@
+package Interface;
+
+import javax.swing.*;
+
+public class PagoReadingForm {
+    private JTextField textField1;
+    private JButton buscarButton;
+    private JButton modificarButton;
+    private JButton button1;
+    private JTable table1;
+}
