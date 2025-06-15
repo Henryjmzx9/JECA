dcacfe4b91142acb456c4a7a7c7d6985f02e26f5
Anderson Javier Galicia Ramírez <gr25004@esfe.agape.edu.sv>
2025-06-15 02:04:57 -0600

    Se modifico el formularioReadingdeClientes

--- src/main/java/Interface/clientReadingForm.java
+++ src/main/java/Interface/clientReadingForm.java
@@ -4,11 +4,10 @@ import javax.swing.*;
 
 public class clientReadingForm {
     private JTextField txtPhoneClient;
-    private JTextField txtEmailClient;
     private JTextField txtAddresClient;
-    private JTextField txtNameClient;
     private JButton btnGuardar;
     private JButton btnEliminar;
     private JButton btnEditar;
     private JTable tableClient;
+    private JButton btnBuscar;
 }
